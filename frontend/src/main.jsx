cb0c231391d6aceb074d892c33dc6241ba352495
tikoyak128-spec <tikoyak128@gmail.com>
2026-09-24 00:44:25 +0700

    Set global default axios baseURL to live Render API

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 import { PrefsProvider } from './context/PrefsContext'
+import axios from "axios";
 
+// Automatically route all default axios calls to the live Render backend
+axios.defaults.baseURL = "https://team3-cinema-management.onrender.com";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
